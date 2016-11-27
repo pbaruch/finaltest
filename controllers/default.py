bcd704b73fd8d15a30e0a56832db301c8cafecda
pbaruch <pbaruch@maxit.com>
2016-11-27 12:01:21 -0500

    first comit

---
+++ controllers/default.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+#########################################################################
+
+
+def display_your_form():
+    form = SQLFORM(db.register)
+    return dict(form=form)
